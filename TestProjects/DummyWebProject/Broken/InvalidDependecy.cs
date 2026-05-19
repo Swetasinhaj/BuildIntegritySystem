accea8277ff48560fc3b82ae0d799eb622e30a10
Swetasinhaj <swetasinhaj@gmail.com>
2026-05-19 13:24:27 +0530

    Add files via upload

---
+++ TestProjects/DummyWebProject/Broken/InvalidDependecy.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DummyProject.Broken
+{
+    public class InvalidDependecy : Controller
+    {
+        public class InvalidDependency
+        {
+            public void Test()
+            {
+                MissingClass obj = new MissingClass();
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
