df2e8f83feedf46fd8067815f5e8f9568307bba6
Swetasinhaj <swetasinhaj@gmail.com>
2026-05-19 15:21:56 +0530

    Update Program.cs

--- BuildIntegrityAnalyzer/Program.cs
+++ BuildIntegrityAnalyzer/Program.cs
@@ -10,7 +10,7 @@ Console.Write("Enter project (.csproj) path: ");
 //string projectPath =@"BuildIntegrityAnalyzer/BuildIntegrityAnalyzer.csproj";
 
 //string? projectPath = Console.ReadLine();
-string projectPath = @"E:\IntelligentBuildIntegritySystem\DummyProject\DummyProject\DummyProject.csproj";
+string projectPath = @"..\TestProjects\DummyWebProject\DummyProject.csproj";
 
 if (string.IsNullOrWhiteSpace(projectPath))
 {
